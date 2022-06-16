3d218f2c0a8e71358b80a3d6ab18f704132460db
Tcharlyson <tcharlyson@codebuds.com>
2022-06-16 10:20:17 +0200

    fix: link with internationalization

--- layouts/default.vue
+++ layouts/default.vue
@@ -11,7 +11,7 @@
         <v-list-item
           v-for="(item, i) in items"
           :key="i"
-          :to="item.to"
+          :to="localePath(item.to)"
           router
           exact
         >
